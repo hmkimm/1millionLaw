17fd504dd666adb107a7273ad870db5c70a501f3
hmkimm <lisamin7@naver.com>
2023-05-11 13:50:09 +0900

    입력값 출력

---
+++ src/components/Second/Second.jsx
@@ -0,0 +1,37 @@
+import "./Second.css";
+
+import React from "react";
+
+export default function Second(props) {
+  return (
+    <div className="second">
+      <span>나는</span>
+      <input
+        className="input"
+        type="text"
+        placeholder="예)프로그래밍"
+        value={props.inp}
+        onChange={(e) => {
+          props.setInp(e.target.value);
+          console.log(props.inp);
+        }}
+      />
+      <span>전문가가 될 것이다.</span>
+      <br />
+      <span>그래서 앞으로 매일 하루에</span>
+      <input
+        className="input input-second"
+        type="number"
+        min={0}
+        step={1}
+        placeholder="예)5시간"
+        onChange={(e) => {
+          props.setNumInp(e.target.value);
+          let division = 100 / props.numInp;
+          console.log(division, "나누기");
+        }}
+      />
+      <span>시간씩 훈련할 것이다.</span>
+    </div>
+  );
+}
